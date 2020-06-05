aa10a4bbbf11af6766d5f6079f0de974bd4fa4eb
陶越 <njnytaoyue@yahoo.co.jp>
2020-06-05 21:16:55 +0800

    update

--- 正则练习.py
+++ 正则练习.py
@@ -1,7 +1,15 @@
 import re
 
-phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
-mo = phoneNumRegex.search('My number is 415-555-4242.')
-print(mo.group(1))
+# phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+# mo = phoneNumRegex.search('My number is 415-555-4242.')
+# print(mo.group(1))
 
 
+# pattern = re.compile(r'(Na){0,3}')
+
+# result = pattern.search('NaNaNa')
+# print(result.group(1))
+
+greedyHaRegex = re.compile(r'(Ha){3,5}?')
+mo1 = greedyHaRegex.search('HaHaHaHaHa')
+print(mo1.group())
