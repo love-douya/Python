673b7b1762325ad80c9b0f328db3ac7eb3995e39
love-douya <njnytaoyue@yahoo.co.jp>
2020-06-23 20:54:58 +0800

    update

--- 正则练习.py
+++ 正则练习.py
@@ -10,6 +10,31 @@ import re
 # result = pattern.search('NaNaNa')
 # print(result.group(1))
 
-greedyHaRegex = re.compile(r'(Ha){3,5}?')
-mo1 = greedyHaRegex.search('HaHaHaHaHa')
-print(mo1.group())
+# greedyHaRegex = re.compile(r'(Ha){3,5}?')
+# mo1 = greedyHaRegex.search('HaHaHaHaHa')
+# print(mo1.group())
+
+# import pyperclip
+
+# emailRegex = re.compile(r'''(
+#     [a-zA-Z0-9._%+-]+ # username
+#     @                 # @ symbol
+#     [a-zA-Z0-9.-]+    # domain name
+#     (\.[a-zA-Z]{2,4}) # dot-something
+# )''', re.VERBOSE)
+
+# text = str(pyperclip.paste())
+# match = []
+
+# for groups in emailRegex.findall(text):
+#     match.append(groups[0])
+
+# # Copy results to the clipboard.
+# if len(match) > 0:
+#     pyperclip.copy('\n'.join(match))
+#     print('Copied to clipboard:')
+#     print('\n'.join(match))
+# else:
+#     print('No phone numbers or email addresses found.')
+
+
